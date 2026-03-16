41470c0a802a447e0bc60e7cdbc34c16ca6f7b01
Younes Jaaidi <younes@marmicode.io>
2026-03-16 23:15:59 +0100

    test: ✅ disable form test on stackblitz due to preview locator issue with fake timers

--- apps/demo/src/app/debounce/cookbook-filter-form.browser.spec.ts
+++ apps/demo/src/app/debounce/cookbook-filter-form.browser.spec.ts
@@ -1,10 +1,13 @@
 import { outputBinding } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { describe, expect, it, onTestFinished, vi } from 'vitest';
-import { page } from 'vitest/browser';
+import { page, server } from 'vitest/browser';
 import { CookbookFilter, CookbookFilterForm } from './cookbook-filter-form.ng';
 
-describe(CookbookFilterForm, () => {
+/* Preview mode is affected by the fake timers.
+ * As of 4.1.0, Locator#fill will never resolve.
+ * Cf. https://github.com/vitest-dev/vitest/issues/9882 */
+describe.skipIf(server.provider === 'preview')(CookbookFilterForm, () => {
   it('does not emit filterChange while debounce is pending', async () => {
     const { filterChangeSpy, keywordsInput } = await mountFilterForm();
 
